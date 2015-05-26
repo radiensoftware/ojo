8906296585094db5e649b5f20c1d5296eaae713a
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-05-26 13:25:08 +0530

    sh

---
+++ 095-java-package-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd $HOME
+
+
+URL0=ftp://10.10.10.10/pub/java
+FILE=jdk-6u45-linux-x64-rpm.bin
+URL=$URL0/$FILE
+/bin rm -f $FILE
+wget $URL
+chmod +x $FILE
+sudo ./$FILE
+type java
+ls -l /usr/java/default
