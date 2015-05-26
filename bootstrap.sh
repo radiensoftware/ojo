0d1d8b6486f95972a30940f70e7978aa7c28cb95
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-05-26 17:13:46 +0530

    sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash -xvf
+echo $PATH
 FILE=DaddyLabs-FTP.repo
 PATH=/etc/yum.repos.d
 PATHNAME=$PATH/$FILE
+/bin/rm -f $PATHNAME
+/bin/touch $PATHNAME
 echo '[daddylabs]'					>> $PATHNAME
 echo 'comment="DaddyLabs Anonymous FTP Service"'	>> $PATHNAME
 echo 'name=CentOS-$releasever - Base'			>> $PATHNAME
 echo 'baseurl=ftp://10.10.10.10/pub/centos/$releasever/os/$basearch/'	>> $PATHNAME
 echo 'gpgcheck=0'					>> $PATHNAME
 echo 'enabled=1'					>> $PATHNAME
-yum -y --disableplugin=fastestmirror git wget named ntp
+/usr/bin/yum -y --disablerepo=base --disablerepo=updates --disableplugin=fastestmirror install git wget named ntp
