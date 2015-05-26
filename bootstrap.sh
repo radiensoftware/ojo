8233684d8dc070feb973246641ed7d5adf062dab
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-05-26 17:29:21 +0530

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,12 @@ echo 'enabled=1'					>> $PATHNAME
 /usr/bin/yum -y --disablerepo=base --disablerepo=updates --disableplugin=fastestmirror install git wget named ntp
 echo '' 				>> /etc/sudoers
 echo '%wheel ALL=(ALL) NOPASSWD: ALL'	>> /etc/sudoers
+
+/bin/rm -f /home/hadoop/first.sh
+/bin/touch /home/hadoop/first.sh
+echo '#!/bin/bash'						>> /home/hadoop/first.sh
+echo 'export PATH=$PATH:/usr/bin'				>> /home/hadoop/first.sh
+echo 'git clone https://github.com/radiensoftware/ojo.git'	>> /home/hadoop/first.sh
+/bin/chmod +x /home/hadoop/first.sh
+/bin/chown hadoop:hadoop /home/hadoop/first.sh
+/bin/su -l hadoop -c /home/hadoop/first.sh
