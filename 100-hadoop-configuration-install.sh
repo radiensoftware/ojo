757b46c35d6360e3f44d2f315dc569e1c10a573d
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-05-26 11:32:28 +0530

    shell

---
+++ 100-hadoop-configuration-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -f bashrc          /home/hadoop/.bashrc
+cp -f hadoop-env.sh   /home/hadoop/hadoop-1.2.1/conf
+cp -f core-site.xml   /home/hadoop/hadoop-1.2.1/conf
+cp -f mapred-site.xml /home/hadoop/hadoop-1.2.1/conf
+cp -f hdfs-site.xml   /home/hadoop/hadoop-1.2.1/conf
