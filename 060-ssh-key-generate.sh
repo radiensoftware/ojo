e106e0c9fba924383e7c0a05818920e0f7290ef1
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-05-26 13:49:31 +0530

    sh

---
+++ 060-ssh-key-generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/expect
+set timeout 20
+spawn "/usr/bin/ssh-keygen"
+expect "id_rsa):" 		{ send "\r" }
+expect "verwrite (y/n)?"	{ send "y\r" }
+expect "no passphrase):" 	{ send "\r" }
+expect "passphrase again:" 	{ send "\r" }
