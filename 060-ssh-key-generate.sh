0e8cc135d9ac21fdb05183e11c5ad389d530e67e
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-27 07:48:24 +0000

    sh

--- 060-ssh-key-generate.sh
+++ 060-ssh-key-generate.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 /bin/rm -f $HOME/.ssh/id_rsa
 /bin/rm -f $HOME/.ssh/id_rsa.pub
-./060-ssh-key-generate.expect
-/bin/chmod +x ssh-copy-id-host-helper.expect
-./ssh-copy-id-host-helper.expect 127.0.0.1
-./ssh-copy-id-host-helper.expect localhost
+./060-ssh-key-generate.x
