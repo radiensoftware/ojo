d4145fb41286be7744ddb9144378c0e6329ec555
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-27 09:22:19 +0000

    hadoop

---
+++ hadoop-create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PATH=$PATH:.
+
+040-rpmforge-htop-git-install.sh
+058-localhost-ipv6-remove.sh
+059-expect-package-install.sh
+060-ssh-key-generate.sh
+061-ssh-copy-id-localhost.sh
+062-ssh-copy-id-this-host.sh
+095-java-package-install.sh
+097-hadoop-package-install.sh
+100-hadoop-configuration-install.sh
+200-epel-repo-install.sh
+300-atop-install.sh
+
+echo "Run "exec bash" to pick up update environment variables"
