c7a158a838a6bbc21ebf7b2756f4608efd014222
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-27 08:26:22 +0000

    hadoop

--- 095-java-package-install.sh
+++ 095-java-package-install.sh
@@ -6,6 +6,8 @@ URL0=ftp://10.10.10.10/pub/java
 FILE=jdk-6u45-linux-x64-rpm.bin
 URL=$URL0/$FILE
 /bin/rm -f $FILE
+/bin/rm -f sun-java*.rpm
+/bin/rm -f jdk*.rpm
 wget $URL
 chmod +x $FILE
 sudo ./$FILE
