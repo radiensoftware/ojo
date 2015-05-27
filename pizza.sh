b17d72fa5810de800f0e8ce1e8128881625c32b0
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-27 09:05:02 +0000

    pizza

--- pizza.sh
+++ pizza.sh
@@ -4,6 +4,6 @@ do
 	date
 	hadoop fs -rmr Pi*
 	sleep 10
-	hadoop /home/hadoop/hadoop-1.2.1/hadoop-examples-1.2.1.jar pi 1000 1000000
+	hadoop jar /home/hadoop/hadoop-1.2.1/hadoop-examples-1.2.1.jar pi 1000 1000000
 	sleep 10
 done
