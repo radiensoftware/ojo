9d294b3764a6ee2dcbcd4493696a41e127cea92c
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-29 10:48:56 +0000

    097-hadoop-package-install.sh

--- 097-hadoop-package-install.sh
+++ 097-hadoop-package-install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -xvf
 cd $HOME
 DIR=hadoop-1.2.1
 SUFFIX=-bin.tar.gz
 FILE=$DIR$SUFFIX
 URL0=ftp://10.10.10.10/hadoop
 URL=$URL0/$FILE
-/bin rm -f $DIR $FILE $FILE.[0-9]*
+/bin/rm -fr $DIR $FILE $FILE.*
 /usr/bin/wget $URL
 sleep 1
 /bin/tar xvf $FILE
