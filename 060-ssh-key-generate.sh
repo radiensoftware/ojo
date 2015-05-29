1640bd361e0d17b8c930948fec36d67d893d461e
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-29 12:07:50 +0000

    ssh

--- 060-ssh-key-generate.sh
+++ 060-ssh-key-generate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+unset SSH_ASKPASS
+unset SSH_AUTH_SOCK
 /bin/rm -f $HOME/.ssh/id_rsa
 /bin/rm -f $HOME/.ssh/id_rsa.pub
 ./helper-ssh-key-generate
--- 062-ssh-copy-id-this-host.sh
+++ 062-ssh-copy-id-this-host.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+unset SSH_ASKPASS
+unset SSH_AUTH_SOCK
 for ip in `./helper-my-ip-addresses`
 do
 	if [ "$ip" != "127.0.0.1" ];
