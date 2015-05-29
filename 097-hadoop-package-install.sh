229ef03e63877f0d867e941485cac7c16e593c71
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-05-29 10:20:01 +0000

    sh

--- 097-hadoop-package-install.sh
+++ 097-hadoop-package-install.sh
@@ -3,7 +3,7 @@ cd $HOME
 DIR=hadoop-1.2.1
 SUFFIX=-bin.tar.gz
 FILE=$DIR$SUFFIX
-URL0=ftp://10.10.10.10/pub/hadoop
+URL0=ftp://10.10.10.10/hadoop
 URL=$URL0/$FILE
 /bin rm -f $DIR $FILE $FILE.[0-9]*
 wget $URL
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ PATHNAME=$PATH/$FILE
 echo '[daddylabs]'					>> $PATHNAME
 echo 'comment="DaddyLabs Anonymous FTP Service"'	>> $PATHNAME
 echo 'name=CentOS-$releasever - Base'			>> $PATHNAME
-echo 'baseurl=ftp://10.10.10.10/pub/centos/$releasever/os/$basearch/'	>> $PATHNAME
+echo 'baseurl=ftp://10.10.10.10/centos/$releasever/os/$basearch/'	>> $PATHNAME
 echo 'gpgcheck=0'					>> $PATHNAME
 echo 'enabled=1'					>> $PATHNAME
 /usr/bin/yum -y --disablerepo=base --disablerepo=updates --disableplugin=fastestmirror install git wget named ntp
