2e39ffd47e968837c5f7a9972668acd23165821a
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-06-02 17:34:58 +0000

    python-install.sh

---
+++ python-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Python 3 Installation
+wget http://www.python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2
+tar jxvf Python-3.3.2.tar.bz2 
+cd Python-3.3.2
+./configure --prefix=/usr/local
+make
+sudo make install
+type python
+type python3
