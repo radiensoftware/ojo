e98fca855c4bef38c2a2e77526818631b4ad549c
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-06-19 13:45:21 +0530

    hadoop

--- hadoop/200-hadoop-master-configure-masters.sh
+++ hadoop/200-hadoop-master-configure-masters.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 ansible namenode -m copy -a "src=multinode/hadoop-1.2.1/conf/masters dest=/home/hadoop/hadoop-1.2.1/conf/masters"
-# ansible namenode -m file -a "dest=/home/hadoop/hadoop-1.2.1/conf/masters state=absent"
