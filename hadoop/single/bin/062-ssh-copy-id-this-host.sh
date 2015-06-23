5c36a628d19c7b404b00124e18b1540465c9858d
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-06-23 06:28:09 +0000

    hadoop

--- hadoop/single/bin/062-ssh-copy-id-this-host.sh
+++ hadoop/single/bin/062-ssh-copy-id-this-host.sh
@@ -18,7 +18,7 @@
 HELPER_HOME=$HOME/ojo/common
 unset SSH_ASKPASS
 unset SSH_AUTH_SOCK
-for ip in `./helper-my-ip-addresses`
+for ip in `$HELPER_HOME/helper-my-ip-addresses`
 do
 	if [ "$ip" != "127.0.0.1" ];
 	then
