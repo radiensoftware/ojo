eebd10779d85cd68b1bb9075b1c2e40ef261dff7
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-06-24 10:28:59 +0530

    keys

---
+++ keys/ssh-keys-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2015. Dinesh Thirumurthy. All Rights Reserved.
+#
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+cat *.pub >> ~/.ssh/authorized_keys
+cat *.pub >> ~/.ssh/authorized_keys2
+chmod 600 ~/.ssh/authorized_keys[2]
+ls -l ~/.ssh
+chmod 700 ~/.ssh
+ls -ld ~/.ssh
