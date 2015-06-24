8186c6b1e44bba8140c9f86035a969c67d96abfe
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-06-24 22:36:56 +0000

    ganglia

--- ganglia/g.sh
+++ ganglia/g.sh
@@ -6,3 +6,5 @@ H=`hostname | sed -e 's/.bit.daddylabs.com//' -e 's/.daddylabs.com//'`
 sed -e 's/template/'$H'/g' $HOME/ojo/ganglia/template.gmond.conf > $H.gmond.conf
 sudo cp -f $HOME/ojo/ganglia/$H.gmond.conf /etc/ganglia/gmond.conf
 sudo service gmond restart
+sudo chkconfig gmond on
+sudo chkconfig gmond --list
