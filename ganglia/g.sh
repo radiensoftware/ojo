44062ae77c5f5472e3e8fc4da316d5f79e3df94c
Dinesh Thirumurthy <dinesh.thirumurthy@acm.org>
2015-06-24 22:27:48 +0000

    ganglia

--- ganglia/g.sh
+++ ganglia/g.sh
@@ -2,7 +2,7 @@
 git pull
 sudo yum install -y ganglia-gmond
 H=`hostname | sed -e 's/.bit.daddylabs.com//' -e 's/.daddylabs.com//'`
-/bin/rm -f $h.gmond.conf
-sed -e 's/template/'$h'/g' $HOME/ojo/ganglia/template.gmond.conf > $h.gmond.conf
+/bin/rm -f $H.gmond.conf
+sed -e 's/template/'$H'/g' $HOME/ojo/ganglia/template.gmond.conf > $H.gmond.conf
 sudo cp -f $HOME/ojo/ganglia/$H.gmond.conf /etc/ganglia/gmond.conf
 sudo service gmond restart
